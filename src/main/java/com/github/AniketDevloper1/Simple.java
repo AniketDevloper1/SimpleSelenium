279c277431c3560da9af520fcbc0e266e2d005c6
aniket wadkar <aniket@vedaitconsulting.co.uk>
2018-01-24 09:53:31 +0000

    hi

--- src/main/java/com/github/AniketDevloper1/Simple.java
+++ src/main/java/com/github/AniketDevloper1/Simple.java
@@ -1,4 +1,21 @@
 package com.github.AniketDevloper1;
 
+import org.openqa.selenium.WebDriver;
+
+import java.util.logging.Logger;
+
 public class Simple {
+    WebDriver driver;
+    Logger log= Logger.getLogger("SimpleSelenium");
+
+
+    public void test(){
+        log.info("This should print out ");
+        log.warning("WARNING ");
+
+    }
+
+    public static void main(String[] args) {
+        new Simple().test();
+    }
 }
